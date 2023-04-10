0155de6cfff092892a384e440eb18a17c09eebb4
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-04-10 08:41:38 +0200

    Write algorithm for rock, paper, scissors game

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+
+Algorithm for rock, paper, scissors game
+
+// Declare function getComputerChoice to get computer's selection
+// Define variable computerSelection to store computer's selection
+// Declare function getPlayerchoice to get player's selection
+// Define variable playerSelection to store user's selection
+// Prompt user for a selection
+// Verify correct input for selection
+// If bad input, alert user and prompt for a correct input
+// If input is good, store it in variable playerSelection
+// Delcare function compareChoice to determine winner of the game
+// Compare computerSelection and playerSelection
+// If computerSelection beats playerSelection, alert user of a loss
+// If computerSelection === playerSelection, alert user of a tie
+// If computerSelection loses to playerSelection, alert user of a win
+
+*/
+
+
