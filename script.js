2483a05e8a6d5f26ffc0bc89bac9c36e77ce3888
itsdavehimself <23064280+itsdavehimself@users.noreply.github.com>
2023-04-10 13:06:35 +0200

    Fix function getPlayerChoice
    Function was not returning playerSelection variable with correct
    value.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 /*
-
 Algorithm for rock, paper, scissors game
-
 // Declare function getComputerChoice to get computer's selection
 // Define variable computerSelection to store computer's selection
 // Declare function getPlayerchoice to get player's selection
@@ -15,47 +13,43 @@ Algorithm for rock, paper, scissors game
 // If computerSelection beats playerSelection, alert user of a loss
 // If computerSelection === playerSelection, alert user of a tie
 // If computerSelection loses to playerSelection, alert user of a win
-
 */
 
+/*
 //Declare function getComputerChoice to get computer's selection
 function getComputerChoice() {
     const random = Math.floor(Math.random()*10) + 1; // Create random number (1-10) to generate rock, paper, scissors selection
-    const computerSelection = null;
     console.log(random);
     if (random <= 3) {
         console.log("rock"); // If number is <= 3 generate "Rock"
-        computerSelection = "rock";
-        return computerSelection;
     } else if (random <= 6) {
         console.log("paper"); // If number is <= 6 generate "Paper"
-        computerSelection = "paper";
-        return computerSelection;
     } else if (random <= 9) {
         console.log("scissors"); // If number is <= 9 generate "Scissors"
-        computerSelection = "scissors";
-        return computerSelection;
     } else {
         getComputerChoice(); // If number is 10, run the function again to get another number
     }
 
 }
+*/
 
 // Declare function getPlayerChoice to get player's selection
 function getPlayerChoice() {
     const playerInput = prompt("Enter rock, paper, or scissors:"); // Prompt user for entry
-    const playerSelection = playerInput.toLowerCase(); // Convert input string to lowercase
-    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
-        console.log(playerSelection); // If user input is "rock", "paper", or "scissors", store entry in variable playerSelection
-        return playerSelection;
+    const playerInputLower = playerInput.toLowerCase(); // Convert input string to lowercase
+    let playerSelection;
+    if (playerInputLower === "rock" || playerInputLower === "paper" || playerInputLower === "scissors") {
+        playerSelection = playerInputLower; // If user input is "rock", "paper", or "scissors", store entry in variable playerSelection
     } else {
         alert("Not a valid entry!"); // If input is not "rock", "paper", or "scissors", alert user
         getPlayerChoice(); // Run function again to ask for correct input
     }
+    console.log(playerSelection);
+    return playerSelection;
 }
 
-getPlayerChoice();
-getComputerChoice();
+
+
 
 
 // Declare function to compare the inputs of user & computer
